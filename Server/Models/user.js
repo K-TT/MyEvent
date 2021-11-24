7f0558b711410df6f8cadae3a4f046eab0d11631
yerim926 <yerim926@users.noreply.github.com>
2021-11-23 21:22:38 -0500

    edited register page and user model

--- Server/Models/user.js
+++ Server/Models/user.js
@@ -48,7 +48,7 @@ let eventSchema = new mongoose.Schema({
 });
 
 // User Schema
-let UserModel = mongoose.Schema(
+let User = mongoose.Schema(
     {
       username: 
       {
@@ -117,4 +117,4 @@ let options = { missingPasswordError: "Incorrect / Missing Password" };
 
 UserModel.plugin(passportLocalMongoose, options);
 
-module.exports = mongoose.model("User", UserModel);
+module.exports.User = mongoose.model("User", User);
