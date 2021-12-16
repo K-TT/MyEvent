93594581b566302fda5ac148589bcce82bf083b7
oksana117 <81168349+oksana117@users.noreply.github.com>
2021-12-16 14:50:01 -0500

    Pop events bug fix

---
+++ .history/test/indexTest_20211214201237.js
@@ -0,0 +1,20 @@
+let assert = require('chai').assert;
+let index = require('../Server/Controllers/index');
+
+const { interestedCounter } = require('../Server/Controllers/index');
+var expect = require('chai').expect;
+
+/*
+describe('Index', function(){
+    it('App should navigate to home page', function(){
+        assert.equal(index, 'hello');
+    });
+});*/
+
+/* Unit test to check if popular events interest counter is more than 5 */
+describe('the pop even function', () => {
+      it('interestedCounter should be more than 5', () => {
+          const result =interestedCounter > 1;
+         expect(result);
+      });
+});
